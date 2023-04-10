283c870a317d5b98e67a90815d5e29ce251eaf18
charlestacda <96652382+charlestacda@users.noreply.github.com>
2023-04-10 11:49:47 +0800

    Add files via upload

---
+++ TempConvert/Converter.cs
@@ -0,0 +1,13 @@
+﻿class Converter
+{
+    public double convertTemp(double num1)
+    {
+        double cel, frh;
+        cel = num1;
+        frh = (cel * 9.0) / 5.0 + 32;
+        return frh;
+
+    }
+
+
+}
---
+++ TempConvert/Tacda_ASS2.cs
@@ -0,0 +1,17 @@
+﻿class Test
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Convert temperature in Celsius to Fahrenheit");
+        Console.Write("Enter temperature in Celsius: ");
+        double cel, frh;
+
+        Converter convert = new Converter();
+        cel = Convert.ToDouble(Console.ReadLine());
+        frh = convert.convertTemp(cel);
+        Console.WriteLine("The temperature in Fahrenheit: {0}", frh);
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadLine();
+
+    }
+}
